6acfffed00cfe47cb693239cbfbf1e37ec02cf83
Will Holtz <wholtz@gmail.com>
2021-04-23 09:31:43 -0700

    make update.sh work with macOS sed

--- update.sh
+++ update.sh
@@ -10,19 +10,19 @@ fi
 VERSION="$1"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sed -i "s/^ARG VERSION=.*$/ARG VERSION=${VERSION}/" Dockerfile
+sed -i.bak "s/^ARG VERSION=.*$/ARG VERSION=${VERSION}/" "${SCRIPT_DIR}/Dockerfile"
+rm  "${SCRIPT_DIR}/Dockerfile.bak"
 
 for f in $(find "${SCRIPT_DIR}/examples" -name Dockerfile); do
-  sed -i  "s%^FROM mambaorg/micromamba:.*$%FROM mambaorg/micromamba:${VERSION}%" "$f"
+  sed -i.bak  "s%^FROM mambaorg/micromamba:.*$%FROM mambaorg/micromamba:${VERSION}%" "$f"
+  rm "$f.bak"
 done
 
-sed -i "s%mambaorg/micromamba:.*$%mambaorg/micromamba:${VERSION}%" "${SCRIPT_DIR}/README.md"
-
-podman build --format docker -t "micromamba:${VERSION}" .
+sed -i.bak "s%mambaorg/micromamba:.*$%mambaorg/micromamba:${VERSION}%" "${SCRIPT_DIR}/README.md"
+rm "${SCRIPT_DIR}/README.md.bak"
 
 git add README.md $(find . -name Dockerfile)
 git commit -m "micromamba v${VERSION}"
 git push origin
 git tag -a "v${VERSION}" -m "micromamba v${VERSION}"
 git push origin --tags
-
