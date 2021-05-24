b8458a96b5981000021e1620ae925729cc0faf98
Will Holtz <wholtz@gmail.com>
2021-05-24 08:45:02 -0700

    Streamline update.sh and catch all Dockerfiles

--- update.sh
+++ update.sh
@@ -10,10 +10,9 @@ fi
 VERSION="$1"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sed -i.bak "s/^ARG VERSION=.*$/ARG VERSION=${VERSION}/" "${SCRIPT_DIR}/Dockerfile"
-rm  "${SCRIPT_DIR}/Dockerfile.bak"
+DOCKERFILES=$(find . -not -path "./test/bats/*" -name '*Dockerfile')
 
-for f in $(find "${SCRIPT_DIR}/*/" -name 'Dockerfile*'); do
+for f in $DOCKERFILES; do
   sed -i.bak  "s%^FROM mambaorg/micromamba:.*$%FROM mambaorg/micromamba:${VERSION}%" "$f"
   rm "$f.bak"
 done
@@ -21,7 +20,7 @@ done
 sed -i.bak "s%mambaorg/micromamba:.*$%mambaorg/micromamba:${VERSION}%" "${SCRIPT_DIR}/README.md"
 rm "${SCRIPT_DIR}/README.md.bak"
 
-git add README.md $(find . -name Dockerfile)
+git add README.md $DOCKERFILES
 git commit -m "micromamba v${VERSION}"
 git push origin
 git tag -a "v${VERSION}" -m "micromamba v${VERSION}"
