bfc7ffb018f9641fbd0f7b5feb5e0c4464c8fe48
Will Holtz <wholtz@gmail.com>
2021-12-09 10:01:24 -0800

    add --commit flag to update.sh (#67)

--- update.sh
+++ update.sh
@@ -2,20 +2,56 @@
 
 set -euf -o pipefail
 
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 version"
+function display_help {
+  echo ""
+  echo -e "Usage: $0 [options] version"
+  echo ""
+  echo "   version                 version number of micromamba to include in image (required)"
+  echo "   -h, --help              show this command reference"
+  echo "   -c, --commit            make a git commit, tag it, and push to origin "
+  echo ""
+}
+
+function unknown_param {
+  echo ""
+  echo "ERROR: Unknown parameter passed: $1"
+  display_help
+  exit 2
+}
+
+if [[ $# -eq 0 ]]; then
+  display_help
   exit 1
 fi
 
-VERSION="$1"
+VERSION=""
+COMMIT=""
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -c|--commit) COMMIT="TRUE" ;;
+    -h|--help) display_help; exit 0 ;;
+    -*) unknown_param "$1" ;;
+    *) [ -z "${VERSION}" ] && VERSION="$1" || unknown_param "$1" ;;
+  esac
+  shift
+done
+
+if [ -z "${VERSION}"  ]; then
+  echo $'\nERROR: No version value passed'
+  display_help
+  exit 3
+fi
+
 BRANCH="release${VERSION}"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DOCKERFILES=$(find . -not -path "./test/bats/*" -name '*Dockerfile')
 
-git checkout main
-git pull
-git checkout -b "$BRANCH"
+if [ "$COMMIT" = "TRUE" ]; then
+  git checkout main
+  git pull
+  git checkout -b "$BRANCH"
+fi
 
 for f in $DOCKERFILES; do
   sed -i.bak  "s%^FROM mambaorg/micromamba:.*$%FROM mambaorg/micromamba:${VERSION}%" "$f"
@@ -28,8 +64,10 @@ rm "${SCRIPT_DIR}/Dockerfile.bak"
 sed -i.bak "s%mambaorg/micromamba:.*$%mambaorg/micromamba:${VERSION}%" "${SCRIPT_DIR}/README.md"
 rm "${SCRIPT_DIR}/README.md.bak"
 
-git add README.md $DOCKERFILES
-git commit -m "micromamba v${VERSION}"
-git push --set-upstream origin "$BRANCH"
-git tag -a "v${VERSION}" -m "micromamba v${VERSION}"
-git push --set-upstream origin "$BRANCH" --tags
+if [ "$COMMIT" = "TRUE" ]; then
+  git add README.md $DOCKERFILES
+  git commit -m "micromamba v${VERSION}"
+  git push --set-upstream origin "$BRANCH"
+  git tag -a "v${VERSION}" -m "micromamba v${VERSION}"
+  git push --set-upstream origin "$BRANCH" --tags
+fi
