c9bc21614937037235d013a00742bab908a6146d
Ben Mares <services-git-throwaway1@tensorial.com>
2021-12-14 22:51:52 +0100

    Use MAMBA_USER in _entrypoint.sh

--- _entrypoint.sh
+++ _entrypoint.sh
@@ -5,7 +5,7 @@ set -ef -o pipefail
 # if USER is not set and not root
 if [[ ! -v USER && $(id -u) -gt 0 ]]; then
   # should get here if 'docker run...' was passed -u with a numeric UID
-  export USER="micromamba"
+  export USER="$MAMBA_USER"
   export HOME="/home/$USER"
 fi
 
