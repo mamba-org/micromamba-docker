3753150c88152cce7dce08698ca0bd577c8f7c36
Will Holtz <wholtz@gmail.com>
2021-12-14 13:07:16 -0800

    ensure test suite runs on current micromamba ver (#83)

--- test.sh
+++ test.sh
@@ -3,6 +3,13 @@ set -eu -o pipefail
 
 export DOCKER_BUILDKIT=1
 
+if [ -z "${MICROMAMBA_VERSION}" ]; then
+  export MICROMAMBA_VERSION="$(./check_version.py 2> /dev/null | cut -f1 -d,)"
+fi
+
+# only used for building the micromamba image, not derived images
+MICROMAMBA_FLAGS="--build-arg VERSION=${MICROMAMBA_VERSION}"
+
 FLAGS=
 if which parallel > /dev/null; then
   if [[ $(uname -s) == "Darwin" ]]; then
@@ -10,13 +17,14 @@ if which parallel > /dev/null; then
   else
     NUM_CPUS=$(nproc)
   fi
-  FLAGS="--jobs $NUM_CPUS"
+  FLAGS="${FLAGS} --jobs ${NUM_CPUS}"
   if [ "$NUM_CPUS" -gt "1" ]; then
     PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     # build main test image here so that each *.bats file doesn't do this work in
     # parallel. The *.bats files will still run this docker command, but it will
     # just be a cache hit.
-    docker build --quiet \
+    docker build $MICROMAMBA_FLAGS \
+	         --quiet \
                  --tag=micromamba:test \
                  "--file=${PROJECT_ROOT}/Dockerfile" \
                  "$PROJECT_ROOT" > /dev/null
--- test/mamba-1322-workaround.bats
+++ test/mamba-1322-workaround.bats
@@ -1,7 +1,16 @@
 setup_file() {
     load 'test_helper/common-setup'
     _common_setup
-    docker build --quiet \
+
+    if [ -z "${MICROMAMBA_VERSION}" ]; then
+      export MICROMAMBA_VERSION="$(./check_version.py 2> /dev/null | cut -f1 -d,)"
+    fi
+
+    # only used for building the micromamba image, not derived images
+    MICROMAMBA_FLAGS="--build-arg VERSION=${MICROMAMBA_VERSION}"
+
+    docker build $MICROMAMBA_FLAGS \
+                 --quiet \
                  --tag=micromamba:test \
 		 --file=${PROJECT_ROOT}/Dockerfile \
 		 "$PROJECT_ROOT" > /dev/null
