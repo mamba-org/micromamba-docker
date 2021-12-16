a2ce67dfd4e4504e79a69e3c5a2cf9f294c79d1f
Will Holtz <wholtz@gmail.com>
2021-12-15 23:46:30 -0800

    consolidate reused test code in common-setup.bash (#96)

--- test/test_helper/common-setup.bash
+++ test/test_helper/common-setup.bash
@@ -5,5 +5,17 @@ _common_setup() {
     load 'test_helper/bats-assert/load'
 
     PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
-    PATH="$PROJECT_ROOT/src:$PATH"
+
+    if [ -z "${MICROMAMBA_VERSION+x}" ]; then
+      MICROMAMBA_VERSION="$("${PROJECT_ROOT}/check_version.py" 2> /dev/null | cut -f1 -d,)"
+      export MICROMAMBA_VERSION
+    fi
+    # only used for building the micromamba image, not derived images
+    MICROMAMBA_FLAGS="--build-arg VERSION=${MICROMAMBA_VERSION}"
+
+    docker build $MICROMAMBA_FLAGS \
+                 --quiet \
+                 --tag=micromamba:test \
+		 "--file=${PROJECT_ROOT}/Dockerfile" \
+		 "$PROJECT_ROOT" > /dev/null
 }
