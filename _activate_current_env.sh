a03288e7d24fa063149b3dff6d7b3a542342b32a
Ben Mares <services-git-throwaway1@tensorial.com>
2022-06-14 23:59:45 +0200

    Activate Conda and Mamba if possible

--- _activate_current_env.sh
+++ _activate_current_env.sh
@@ -9,6 +9,22 @@ fi
 # Initialize the current shell
 eval "$("${MAMBA_EXE}" shell hook --shell=bash)"
 
+# Try to initialize Conda in case it is installed.
+__conda_setup="$('conda' 'shell.bash' 'hook' 2> /dev/null)" || true
+if [ ! -z "${__conda_setup}" ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "${CONDA_PREFIX}/etc/profile.d/conda.sh" ]; then
+        . "${CONDA_PREFIX}/etc/profile.d/conda.sh"
+    fi
+fi
+unset __conda_setup
+
+# Initialize Mamba in case it is on the path.
+if [ -f "${CONDA_PREFIX}/etc/profile.d/mamba.sh" ]; then
+    . "${CONDA_PREFIX}/etc/profile.d/mamba.sh"
+fi
+
 # For robustness, try all possible activate commands.
 conda activate "${ENV_NAME}" 2>/dev/null \
   || mamba activate "${ENV_NAME}" 2>/dev/null \
