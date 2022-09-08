421933a7965f0e7a35f4a4a6e0f09512ae6632cf
Will Holtz <wholtz@gmail.com>
2022-09-08 14:16:29 -0700

    run full test suite on pull requests (#204)

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-function-docstring
 
 import glob
-import os
+from pathlib import Path
 
 import nox
 
@@ -12,19 +12,42 @@ PY_VERSION = "3.10"
 def get_base_images(file_name):
     """would be cleaner to use yaml.load here, but want to avoid the dependency"""
     out = []
-    with open(file_name, 'r', encoding='utf-8') as f:
-        for line in f:
+    with open(file_name, "r", encoding="utf-8") as yaml_fh:
+        for line in yaml_fh:
             if line.strip() == "image:":
                 break
-        for line in f:
-            if line.strip().startswith('- '):
+        for line in yaml_fh:
+            if line.strip().startswith("- "):
                 out.append(line.strip()[2:])
             else:
                 break
     return out
 
 
-BASE_IMAGES = get_base_images('.github/workflows/push_latest.yml')
+def get_default_base_image(file_name):
+    with open(file_name, "r", encoding="utf-8") as yaml_fh:
+        for line in yaml_fh:
+            bare_line = line.strip().replace('"', "'")
+            if bare_line.startswith("DEFAULT_BASE_IMAGE:"):
+                return bare_line.split("'")[1]
+    raise ValueError("Did not find DEFAULT_BASE_IMAGE")
+
+
+REPO_DIR = Path(__file__).resolve().parent
+PUSH_WORKFLOW = REPO_DIR / ".github/workflows/push_latest.yml"
+BASE_IMAGES = get_base_images(PUSH_WORKFLOW)
+DEFAULT_BASE_IMAGE = get_default_base_image(PUSH_WORKFLOW)
+assert DEFAULT_BASE_IMAGE in BASE_IMAGES
+
+nox.options.sessions = [
+    f"image_tests(base_image='{DEFAULT_BASE_IMAGE}')",
+    "shellcheck",
+    "pylint",
+    "flake8",
+    "mypy",
+    "black",
+    "pytest",
+]
 
 PYLINT_DEPS = [
     "pylint==2.12.2",
@@ -52,14 +75,19 @@ MYPY_DEPS = [
 
 py_files = set(glob.glob("*.py") + glob.glob("test/*.py")) - {"__init__.py"}
 shell_scripts = set(glob.glob("*.sh") + glob.glob("test/*.bats"))
-cwd = os.getcwd()
 
 
 @nox.session(python=PY_VERSION)
 @nox.parametrize("base_image", BASE_IMAGES)
-def tests(session, base_image):
+def image_tests(session, base_image):
     """Tests generation and use of docker images"""
-    session.run(os.path.join(cwd, "test_with_base_image.sh"), f"{base_image}", external=True)
+    session.run(REPO_DIR / "test_with_base_image.sh", f"{base_image}", external=True)
+
+
+@nox.session(python=PY_VERSION)
+def default_base_image_tests(session):
+    """Tests generation and use of docker images using default base image"""
+    image_tests(session, DEFAULT_BASE_IMAGE)
 
 
 @nox.session(python=PY_VERSION)
@@ -69,8 +97,16 @@ def shellcheck(session):
     try:
         session.run("shellcheck", *inputs, external=True)
     except FileNotFoundError:
-        session.run("docker", "run", "--rm", "-v", f"{cwd}:/mnt",
-                    "koalaman/shellcheck:stable", *inputs, external=True)
+        session.run(
+            "docker",
+            "run",
+            "--rm",
+            "-v",
+            f"{REPO_DIR}:/mnt",
+            "koalaman/shellcheck:stable",
+            *inputs,
+            external=True,
+        )
 
 
 # All sessions defined below here are for testing/linting python code
