f9f707e7fe6110cc5085777e2c1284afb33df267
Will Holtz <wholtz@gmail.com>
2023-07-11 10:01:28 -0700

    docs: update doc version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'micromamba-docker'
 copyright = '2023, micromamba documentation contributors'
 author = 'micromamba documentation contributors'
-release = '1.4.4'
+release = '1.4.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
