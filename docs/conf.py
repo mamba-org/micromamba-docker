650dff43c0538c2dc09debd0cc41f53df7d49d2b
wholtz <7772011+wholtz@users.noreply.github.com>
2025-03-20 00:14:57 +0000

    Update to micromamba version 2.0.8

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'micromamba-docker'
 copyright = '2023-2024, micromamba documentation contributors'
 author = 'micromamba documentation contributors'
-release = '2.0.7'
+release = '2.0.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
