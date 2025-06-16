78683e1a9df5ae04a274f9f11af4fd47de2cc7d8
wholtz <7772011+wholtz@users.noreply.github.com>
2025-06-16 16:16:43 +0000

    Update to micromamba version 2.3.0

--- examples/generate_lock/generate_lock.sh
+++ examples/generate_lock/generate_lock.sh
@@ -1,5 +1,5 @@
 docker run --rm -v "$(pwd):/tmp" \
    "--platform=${DOCKER_PLATFORM}" \
-   mambaorg/micromamba:2.2.0 /bin/bash -c "\
+   mambaorg/micromamba:2.3.0 /bin/bash -c "\
      micromamba create --yes --name new_env --file env.yaml \
      && micromamba env export --name new_env --explicit" > env.lock
